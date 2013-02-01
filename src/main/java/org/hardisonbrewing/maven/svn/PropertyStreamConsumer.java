86ed2d0361c1560f63f455fce4f3bb7578804c78
Martin M Reed <martin.reed@hallowtech.com>
2013-01-31 22:38:11 -0600

    Adding update-tag mojo

---
+++ src/main/java/org/hardisonbrewing/maven/svn/PropertyStreamConsumer.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2013 Martin M Reed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.hardisonbrewing.maven.svn;
+
+import java.util.Properties;
+
+import org.codehaus.plexus.util.cli.StreamConsumer;
+
+public class PropertyStreamConsumer implements StreamConsumer {
+
+    private final Properties properties;
+
+    public PropertyStreamConsumer(Properties properties) {
+
+        this.properties = properties;
+    }
+
+    @Override
+    public void consumeLine( String line ) {
+
+        line = line.trim();
+
+        int indexOf = line.indexOf( ' ' );
+
+        if ( indexOf == -1 ) {
+            return;
+        }
+
+        String key = line.substring( 0, indexOf ).trim();
+        String value = line.substring( indexOf + 1 ).trim();
+        properties.put( key, value );
+    }
+}
