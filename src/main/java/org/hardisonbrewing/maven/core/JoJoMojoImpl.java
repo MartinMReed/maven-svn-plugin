a4e0869b0a5a01fb6c93b50587a2a3ba6cdfa145
Martin M Reed <martin.reed@hallowtech.com>
2013-01-31 19:16:10 -0600

    LICENSE

--- src/main/java/org/hardisonbrewing/maven/core/JoJoMojoImpl.java
+++ src/main/java/org/hardisonbrewing/maven/core/JoJoMojoImpl.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2010-2011 Martin M Reed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.hardisonbrewing.maven.core;
 
 import java.util.List;
@@ -16,7 +32,6 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 import org.codehaus.plexus.context.Context;
 import org.codehaus.plexus.context.ContextException;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
-import org.hardisonbrewing.maven.core.JoJoMojo;
 
 public abstract class JoJoMojoImpl extends JoJoMojo implements Contextualizable {
 
